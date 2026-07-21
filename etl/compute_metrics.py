5fba0f15e54fc1c3487abd19f65cb57d1c72edd1
drayh-luchade <drayhluchade@gmail.com>
2026-07-20 20:45:51 -0400

    Update compute_metrics.py

--- etl/compute_metrics.py
+++ etl/compute_metrics.py
@@ -80,7 +80,7 @@ def main():
         h3id = cell["h3"]
         clat, clng = cell["center"][1], cell["center"][0]
 
-        props = {"h3": h3id, **counts[h3id]}
+        props = {"h3": h3id, "h3_res": resolution, **counts[h3id]}
 
         for league, venues in VENUES.items():
             if venues:
